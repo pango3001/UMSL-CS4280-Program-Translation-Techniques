fe0e6be5a087ffb92d7fb156b6fd64052ec5e038
pango3001 <pango3001@gmail.com>
2021-03-08 02:29:43 -0600

    Update scanner.cpp

--- p1/scanner.cpp
+++ p1/scanner.cpp
@@ -114,7 +114,6 @@ Token scan(std::ifstream& in_file, unsigned int& line_number){
     std::string current_word = "";   
     
     while (current_state < 100){
-        std::cout << "here" << std::endl;
         in_file.get(current_char);
         
         /* Skipping comments */
@@ -155,7 +154,7 @@ Token scan(std::ifstream& in_file, unsigned int& line_number){
         }
 
         next_state = fsa_table[current_state][fsa_column];
-        std::cout << " Next State: " << next_state << std::endl;
+
         if (next_state == 23) {
             std::cout << "SCANNER ERROR 2: Invalid character \"" << current_char << "\"";
             std::cout << " at line: " << line_number << std::endl;
@@ -219,7 +218,6 @@ int setFSAcol(char current_char){
     else  // valid symbol
     {
         if (allowed_symbols.find(current_char) != allowed_symbols.end())
-            //std::cout << " Char: " << allowed_symbols[current_char] << std::endl;
             return allowed_symbols[current_char];
     }
 
