7e915fe61f341f55d5403120ab24d994fe6c0dbd
pango3001 <pango3001@gmail.com>
2021-04-07 13:53:24 -0500

    Update main.cpp

--- testing/main.cpp
+++ testing/main.cpp
@@ -7,27 +7,64 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <ctime>
 
+srand((unsigned)time(NULL));
+/*
+class Omap {
+public:
+    
+    std::string name;
+    int pos[6][6];
+
+    Omap(std::string name, int ozone_level) {
+        this->name = name;
+        this->pos = setLevel(ozone_level);
+
+    }
+
+private:
+    int** setLevels(int Olevel) {
+
+        
+    }
+
+
+
+};
+*/
 int main(int argc, char** argv) {
+
+    std::cout << "Starting OzoneMap" << std::endl;
+
+    // make 2d array
+
     
-    // Pointer to an integer
-    int* p;
-
-    // Pointer to an array of 5 integers
-    int(*ptr)[5];
-    int arr[5];
-
-    // Points to 0th element of the arr.
-    p = arr;
-
-    // Points to the whole array arr.
-    ptr = &arr;
-
-    std::cout << "p =" << p << ", ptr = " << ptr << std::endl;
-    p++;
-    ptr++;
-    std::cout << "p =" << p << ", ptr = " << ptr << std::endl;
     
+    
+    int pos[6][6];
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    std::cout << "Making OzoneMap" << std::endl;
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 6; j++) {
+            pos[i][j] = k;
+            k++;
+        }
+    }
+    std::cout << "printing OzoneMap" << std::endl;
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 6; j++) {
+            std::cout << pos[i][j] << " ";
+            k++;
+        }
+        std::cout << "\n";
+    }
+    
+    
+   
 
     return 0;
 }
