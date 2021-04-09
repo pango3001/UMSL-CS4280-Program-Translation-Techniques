108b8a8f0292c4842a181f4f40568793456b078a
pango3001 <pango3001@gmail.com>
2021-04-08 19:00:10 -0500

    updates

--- p2/scanner.h
+++ p2/scanner.h
@@ -1,9 +1,3 @@
-// Name:    Jesse McCarville-Schueths
-// Course:  4280
-// Date:    Mar 7, 2021
-// Project: P1
-// File:    scanner.h
-
 #ifndef SCANNER_H
 #define SCANNER_H
 #include "token.h"
@@ -11,13 +5,13 @@
 #include <map>
 #include <fstream>
 
-// scanner
-Token scanner(std::ifstream& in_file,unsigned int& lineNum);
+/* Scanner Function */
+Token scan(std::ifstream& in_file, unsigned int &line_number);
 
-// finds col of char in FSA table
-int setFSAcol(char current_char);
+/* FSA Column Getter */
+int get_fsa_column(char current_char);
 
-// sets token
-Token setToken(int state, std::string word, unsigned int lineNum);
+/* Token Getter */
+Token get_token(int state, std::string word, unsigned int line_number);
 
 #endif
