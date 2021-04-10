cb89760cf61b044b01a77056046be9865f9a13fc
pango3001 <pango3001@gmail.com>
2021-04-10 17:53:12 -0500

    Update parser.cpp

--- p2/parser.cpp
+++ p2/parser.cpp
@@ -13,11 +13,13 @@ Token tk;
 /* Array of strings mapped to token_id enum */
 std::string tokes[] = { "BEGIN_TK","END_TK","LOOP_TK","WHILE_TK", "VOID_TK", "EXIT_TK", "GETTER_TK", "OUTTER_TK", "MAIN_TK", "IF_TK", "THEN_TK", "ASSIGN_TK", "DATA_TK", "PROC_TK", "EQUALS_TK", "EQUALS_OR_LESS_THAN_TK", "EQUALS_OR_GREAT_THAN_TK", "EQUALS_EQUALS_TK", "COLON_TK", "COLON_EQUALS_TK", "PLUS_TK", "MINUS_TK", "ASTERISK_TK", "SLASH_TK", "PERCENT_TK", "PERIOD_TK", "LEFT_PAREN_TK", "RIGHT_PAREN_TK", "COMMA_TK", "LEFT_BRACE_TK", "RIGHT_BRACE_TK", "SEMI_COLON_TK", "LEFT_BRACKET_TK", "RIGHT_BRACKET_TK", "ID_TK", "INT_TK", "EOF_TK", "ERROR_TK" };
 
+bool debug = true;
+
 
 /* Main parsing function called from main */
 Node* parser()
 {
-    std::cout << "parser" << "\n";
+    if (debug) {std::cout << "parser" << "\n";}
     /* creating the root node */
     Node* node;
     /* scanning for the first token */
@@ -40,7 +42,7 @@ Node* parser()
 /* Function for <program> -> void <vars> <block> */
 Node* program()
 {
-    std::cout << "program" << "\n";
+    if (debug) {std::cout << "program" << "\n";}
     /* depth of node in tree */
     int depth = 0;
     /* creating a node for <program> */
