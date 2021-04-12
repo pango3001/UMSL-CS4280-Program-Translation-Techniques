f58ca083998013aa3dfb202d5b0d15dcebf18ad9
pango3001 <pango3001@gmail.com>
2021-04-11 21:27:18 -0500

    Update main.cpp

--- p2/main.cpp
+++ p2/main.cpp
@@ -1,102 +1,10 @@
-///* Includes */
-//#include <iostream>
-//#include <string>
-//#include <fstream>
-//#include "parser.h"
-//
-//int main(int argc, char** argv)
-//{
-//    /* Too many arguments */
-//    if (argc > 2)
-//    {
-//        std::cout << "Too many arguments given" << std::endl;
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    /* Filename argument given */
-//    else if (argc == 2)
-//    {
-//        /* Get filenaema and append extension */
-//        std::string file_name = argv[1];
-//        file_name += ".fs18";
-//
-//        std::ofstream outfile;
-//        outfile.open(file_name, std::ios_base::app);
-//        outfile << " ";
-//        
-//        /* Opening the file */
-//        // std::ifstream in_file;
-//        in_file.open(file_name);
-//
-//        /* Given filename cannot be opened */
-//        if (!in_file)
-//        {
-//            std::cout << "Error opening " << file_name << " for reading";
-//            std::cout << std::endl;
-//            exit(EXIT_FAILURE);
-//        }
-//
-//        /* Call the parser */
-//        Node* root = parser();
-//        print_tree(root);        
-//        /* Close the input file */
-//        in_file.close();
-//
-//    }
-//
-//    /* No filename argument given */
-//    else if (argc == 1)
-//    {
-//        /* Creating a temporary file to hold keyboard input */ 
-//        std::ofstream temp_file;
-//        std::string file_name = "keyboard_input.txt";
-//        temp_file.open(file_name, std::ios::trunc);
-//
-//        /* Cannot open temporary file */
-//        if (!temp_file)
-//        {
-//            std::cout << "Error opening " << file_name << " for writing";
-//            std::cout << std::endl;
-//            exit(EXIT_FAILURE);
-//        }
-//
-//        /* Helper string variable */
-//        char current_char;
-//
-//        /* Writing keyboard input to temporary file */
-//        while (std::cin >> std::noskipws >> current_char)
-//            temp_file << current_char;
-//    
-//        /* Closing the file */
-//        temp_file.close();        
-//        
-//        /* Opening keyboard input */
-//        in_file.open(file_name);
-//
-//        /* Cannot open temporary file */
-//        if (!in_file)
-//        {
-//            std::cout << "Error opening " << file_name << " for reading";
-//            std::cout << std::endl;
-//            exit(EXIT_FAILURE);
-//        }
-//        /* TODO */
-//        /* Call the parser */
-//        parser();
-//        /* Close the input file */
-//        in_file.close();
-//    }
-//}
+// Name:    Jesse McCarville-Schueths
+// Course:  4280
+// Date:    April 11, 2021
+// Project: P2
+// File:    main.cpp
 
 
-//
-//// Name:    Jesse McCarville-Schueths
-//// Course:  4280
-//// Date:    Mar 7, 2021
-//// Project: P1
-//// File:    main.cpp
-//
-//
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -147,13 +55,13 @@ int main(int argc, char** argv) {
 
     
 
-    // Scan file
+    // Scan and parse file
     if (in_file) {
         std::cout << "Starting the parser..." << std::endl;
-        Node* root = parser();
-        print_tree(root);        
-        /* Close the input file */
-        in_file.close();
+        Node* root = parser(); // run parser
+        print_tree(root);      // print tree
+        
+        in_file.close(); // close file
 
     }
     else {  // cannot open file
