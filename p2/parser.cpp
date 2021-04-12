614e4e5d179ec2366e966d7f08b4dc26c7040ee0
pango3001 <pango3001@gmail.com>
2021-04-11 22:15:53 -0500

    Update parser.cpp

--- p2/parser.cpp
+++ p2/parser.cpp
@@ -9,8 +9,8 @@ Token tokey; // so many things are named around token so tokey it is
 std::string tokes[] = { "BEGIN_TK","END_TK","LOOP_TK","WHILE_TK", "VOID_TK", "EXIT_TK", "GETTER_TK", "OUTTER_TK", "MAIN_TK", "IF_TK", "THEN_TK", "ASSIGN_TK", "DATA_TK", "PROC_TK", "EQUALS_TK", "EQUALS_OR_LESS_THAN_TK", "EQUALS_OR_GREAT_THAN_TK", "EQUALS_EQUALS_TK", "COLON_TK", "COLON_EQUALS_TK", "PLUS_TK", "MINUS_TK", "ASTERISK_TK", "SLASH_TK", "PERCENT_TK", "PERIOD_TK", "LEFT_PAREN_TK", "RIGHT_PAREN_TK", "COMMA_TK", "LEFT_BRACE_TK", "RIGHT_BRACE_TK", "SEMI_COLON_TK", "LEFT_BRACKET_TK", "RIGHT_BRACKET_TK", "ID_TK", "INT_TK", "EOF_TK", "ERROR_TK" };
 
 // FOR DEBUGGING
-bool debug = false;
-bool debug2 = false;
+bool debug = true;
+bool debug2 = true;
 
 
 // Start and end parser
