e5f1ba5596996557710760527aaadd6bbb4e5015
pango3001 <pango3001@gmail.com>
2021-04-23 14:54:59 -0500

    Update semantics.cpp

--- p3/semantics.cpp
+++ p3/semantics.cpp
@@ -118,7 +118,7 @@ void semantic_check(Node* node, int count)
     }
     else if (node->name == "<expr>")
     {
-        if (node->token_1.token_ID == SLASH_TK || node->token_1.token_ID == ASTERISK_TK)
+        if (node->token_1.token_ID == MINUS_TK)
         {
             if (node->child_1 != nullptr)
                 semantic_check(node->child_1, count);
@@ -130,7 +130,7 @@ void semantic_check(Node* node, int count)
     }
     else if (node->name == "<A>")
     {
-        if (node->token_1.token_ID == PLUS_TK || node->token_1.token_ID == MINUS_TK)
+        if (node->token_1.token_ID == PLUS_TK)
         {
             if (node->child_1 != nullptr)
                 semantic_check(node->child_1, count);
