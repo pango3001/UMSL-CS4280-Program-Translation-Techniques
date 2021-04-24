b47291763d5a09704687e57b9b992b8fbff6269a
pango3001 <pango3001@gmail.com>
2021-04-24 12:19:59 -0500

    Update semantics.cpp

--- p3/semantics.cpp
+++ p3/semantics.cpp
@@ -242,8 +242,8 @@ void semantic_check(Node* node, int count)
 
 void print_stack() {
     for (auto i = 0; i < max_stack_size; i++) {
-        if (stack[i].value == "")break;
-        std::cout << stack[i].value << "";
+        if (stack[i].token_string == "")break;
+        std::cout << stack[i].token_string << "";
     }
     std::cout << std::endl;
 }
