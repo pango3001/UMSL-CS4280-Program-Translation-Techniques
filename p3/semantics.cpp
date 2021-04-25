7cafaed58ee3036f7cb63fa999a4fb6a1ba56d98
pango3001 <pango3001@gmail.com>
2021-04-25 16:44:31 -0500

    Update semantics.cpp

--- p3/semantics.cpp
+++ p3/semantics.cpp
@@ -34,7 +34,7 @@ void push(Token tk) {
 // pops stack
 void pop(int scope_start) {
     for (int i = var_count; i > scope_start; i--){
-        if (debugger2) { std::cout << "Popping \'" << stack[i].token_string << "\' from the stack\n"; }  // for debugging
+        if (debugger2) { std::cout << "Popping \'" << stack[i-1].token_string << "\' from the stack\n"; }  // for debugging
         stack[i].token_string == "";
         var_count--; if (debugger2) { std::cout << "VAR COUNT: " << var_count << "\n"; }  // for debugging
     }
