9707394e69c35e28d6580ccb41682208dbb35f59
pango3001 <pango3001@gmail.com>
2021-04-25 16:45:27 -0500

    Update semantics.cpp

--- p3/semantics.cpp
+++ p3/semantics.cpp
@@ -37,6 +37,7 @@ void pop(int scope_start) {
         if (debugger2) { std::cout << "Popping \'" << stack[i-1].token_string << "\' from the stack\n"; }  // for debugging
         stack[i].token_string == "";
         var_count--; if (debugger2) { std::cout << "VAR COUNT: " << var_count << "\n"; }  // for debugging
+        show_stack();
     }
 }
 
