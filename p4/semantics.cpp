dfe1343396e3947d00c00ae89750072bcfd72cfc
pango3001 <pango3001@gmail.com>
2021-05-07 15:55:39 -0500

    Update semantics.cpp

--- p4/semantics.cpp
+++ p4/semantics.cpp
@@ -134,7 +134,8 @@ void semantic_check(Node* node, int index)
         scope = var_count;
 
         if (tos_distance == -1 || tos_distance > index){
-            VARIABLE.resize(++var_, "NULL");
+            std::string nullString = "NULL";
+            VARIABLE.resize(++var_, nullString);
             var_--;
             VARIABLE[var_].ID = node->token_2.token_string;
             VARIABLE[var_].value = node->token_4.token_string;
